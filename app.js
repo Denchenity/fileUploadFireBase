935317796e862411fef61fab31eec815cf135862
Denchenity <68394756+Denchenity@users.noreply.github.com>
2022-01-31 13:10:37 +0300

    update

--- app.js
+++ app.js
@@ -5,12 +5,7 @@ import 'firebase/compat/storage';
 import { upload } from './upload.js';
 
 const firebaseConfig = {
-        apiKey: "AIzaSyA3idLQz-KCyeBli-LZ8-847u1FKBGIurU",
-        authDomain: "uploadfile-206e2.firebaseapp.com",
-        projectId: "uploadfile-206e2",
-        storageBucket: "uploadfile-206e2.appspot.com",
-        messagingSenderId: "327059104632",
-        appId: "1:327059104632:web:bfbdb82754a96b8cad9e8d"
+        
 };
 
 firebase.initializeApp(firebaseConfig);
